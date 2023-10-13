4568b5ea450c04f75cef5970e2abbd43cd1c2a46
Juan Ramirez <jramirez.uy@gmail.com>
2023-10-12 23:02:51 -0300

    count all files, not only those matching

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -11,7 +11,7 @@ searchstr="$2"
 [[ "$#" -ne 2 ]] && failexit "Usage: finder.sh <filesdir> <searchstr>"
 [[ ! -d "$filesdir" ]] && failexit "Error: $filesdir is not a directory"
 
-files=$(grep -r "$searchstr" "$filesdir" | cut -d: -f1 | uniq | wc -l)
+files=$(find "$filesdir" -type f | wc -l)
 lines=$(grep -r "$searchstr" "$filesdir" | wc -l)
 
 echo "The number of files are $files and the number of matching lines are $lines"
